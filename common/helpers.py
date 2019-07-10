f04519c0b62df1fd0a1dbc50dea2374d9e48f912
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-07-09 18:27:08 -0700

    add status code helpers

--- common/helpers.py
+++ common/helpers.py
@@ -33,6 +33,27 @@ class Oops(Exception):
         self.status_code = status_code
 
 
+#status code helpers taken from https://github.com/flask-api/flask-api/blob/master/flask_api/status.py
+def is_informational(code):
+    return code >= 100 and code <= 199
+
+
+def is_success(code):
+    return code >= 200 and code <= 299
+
+
+def is_redirect(code):
+    return code >= 300 and code <= 399
+
+
+def is_client_error(code):
+    return code >= 400 and code <= 499
+
+
+def is_server_error(code):
+    return code >= 500 and code <= 599
+
+
 def make_jsonapi_error_object(code, error_id=None, title=None, message=None):
     """ Generates a JSON:API error response
 

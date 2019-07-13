2059494da00fcc7de02c98f627b0675787027600
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-07-13 09:51:35 -0700

    add model and schema for BellSchedule

--- common/models.py
+++ common/models.py
@@ -20,3 +20,23 @@ class SchoolModel(object):
 
     def __repr__(self):
         return '<School(name={self.full_name!r}, id={self.identifier!r})>'.format(self=self)
+
+
+class BellScheduleModel(object):
+
+    def __init__(self, identifier, full_name=None, display_name=None, school_id=None, creation_date=None, last_modified=None):
+        self.type = "bellschedule"
+        self.identifier = identifier
+        self.full_name = full_name
+        self.display_name = display_name
+        self.school_id = school_id
+        self.creation_date = creation_date if creation_date is not None else datetime.datetime.now()
+        self.last_modified = last_modified if last_modified is not None else datetime.datetime.now()
+
+    def get_uri_for(self, blueprint_name):
+        # here the second time blueprint_name is called, it is acting like the api version number
+        return url_for(
+            blueprint_name + "." + blueprint_name + "_single_bellschedule", school_id=self.identifier.hex, _external=True)
+
+    def __repr__(self):
+        return '<BellSchedule(name={self.full_name!r}, id={self.identifier!r})>'.format(self=self)
--- common/schemas.py
+++ common/schemas.py
@@ -1,5 +1,5 @@
 from marshmallow import Schema, fields, post_load
-from common.models import SchoolModel
+from common.models import SchoolModel, BellScheduleModel
 
 
 class SchoolSchema(Schema):
@@ -13,3 +13,16 @@ class SchoolSchema(Schema):
     @post_load
     def make_school(self, data):
         return SchoolModel(**data)
+
+
+class BellScheduleSchema(Schema):
+    identifier = fields.UUID(load_from='id', dump_to='id')
+    full_name = fields.Str(allow_none=True)
+    display_name = fields.Str(allow_none=True)
+    school_id = fields.UUID(load_from='school_id', dump_to='school_id')
+    creation_date = fields.DateTime()
+    last_modified = fields.DateTime()
+
+    @post_load
+    def make_bell_schedule(self, data):
+        return BellScheduleModel(**data)

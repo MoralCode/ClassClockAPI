e362703ffe47beb9bb2f279e549c5c5a02265f6d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-07-22 12:01:38 -0700

    add scope constants

--- common/constants.py
+++ common/constants.py
@@ -8,4 +8,11 @@ class AuthType(Enum):
 
 class APIScopes(Enum):
     # Read the name, id, and acronym of every publicly accessible ClassClock school
-    READ_ALL_SCHOOLS = "read:schools-public"
+    READ_SCHOOL_META = "read:school"
+    CREATE_SCHOOL = "create:school"
+    EDIT_SCHOOL = "edit:school"
+    DELETE_SCHOOL = "delete:school"
+    READ_BELL_SCHEDULES = "read:bellschedules"
+    CREATE_BELL_SCHEDULES = "create:bellschedules"
+    EDIT_BELL_SCHEDULES = "edit:bellschedules"
+    DELETE_BELL_SCHEDULES = "delete:bellschedules"

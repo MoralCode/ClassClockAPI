d4f7b7082685d78c97c331431a54d4edff96576c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-06-13 11:43:07 -0700

    update GUID TypeDecorator Types to allow marshmallow validation to be happy

--- common/guid.py
+++ common/guid.py
@@ -4,7 +4,14 @@ from sqlalchemy import types, func
 
 #https://docs.sqlalchemy.org/en/13/core/custom_types.html#backend-agnostic-guid-type
 class HashColumn(types.TypeDecorator):
-    impl=types.BINARY
+    impl=types.CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(UUID())
+        else:
+            return dialect.type_descriptor(types.BINARY(16))
+
 
     def process_bind_param(self, value, dialect):
         if value is None:

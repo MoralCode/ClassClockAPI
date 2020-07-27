bc4465a74f65d9fcf4fdde5f807326c6897ab2bd
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-07-26 18:24:07 -0700

    create custom pluck field to pass down the database session

--- common/schemas.py
+++ common/schemas.py
@@ -4,11 +4,25 @@ Customized Marshmallow-SQLAlchemy and Marshmallow-JSONAPI Schemas to combine Sch
 import marshmallow as ma
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 from marshmallow_sqlalchemy.fields import Nested
+from marshmallow.fields import Pluck
 
 from common.db_schema import db
 
 from common.db_schema import BellSchedule, BellScheduleMeetingTime, School, BellScheduleDate
 
+
+# Modified From https://github.com/marshmallow-code/marshmallow-sqlalchemy/commit/cf996b1f448d9b115b083489c8eb96be3bf1dd40#diff-7e28a06588f9d4acda3f3dd4224899afR136
+class SessionPluck(Pluck):
+    """Pluck field that inherits the session from its parent like Nested does."""
+
+    def _deserialize(self, *args, **kwargs):
+        if hasattr(self.schema, "session"):
+            try:
+                self.schema.session = self.root.session
+            except AttributeError:
+                # Marshmallow 2.0.0 has no root property.
+                self.schema.session = self.parent.session
+        return super(Pluck, self)._deserialize(*args, **kwargs)
 
 class SchoolSchema(SQLAlchemyAutoSchema):
     class Meta:

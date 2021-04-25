e8e01cf5edf56f4505feaf37f9698309bdcf8e8b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-04-24 22:35:24 -0700

    WIP from attempting to make sqlite work

--- common/guid.py
+++ common/guid.py
@@ -1,30 +1,43 @@
-from __future__ import absolute_import
-import uuid
-from sqlalchemy import types, func
+########################
+# UUID for SQLite hack #
+########################
 
-#https://docs.sqlalchemy.org/en/13/core/custom_types.html#backend-agnostic-guid-type
-class HashColumn(types.TypeDecorator):
-    impl=types.CHAR
+from sqlalchemy.types import TypeDecorator, BINARY, CHAR
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+
+class HashColumn(TypeDecorator):
+    """Platform-independent GUID type.
+    Uses PostgreSQL's UUID type, otherwise uses
+    CHAR(32), storing as stringified hex values.
+    """
+    impl = BINARY
 
     def load_dialect_impl(self, dialect):
         if dialect.name == 'postgresql':
             return dialect.type_descriptor(UUID())
         else:
-            return dialect.type_descriptor(types.BINARY(16))
+            return dialect.type_descriptor(BINARY(16))
 
 
     def process_bind_param(self, value, dialect):
         if value is None:
             return value
+        # elif dialect.name == 'postgresql':
+        #     return str(value)
         else:
-            return uuid.UUID(hex=value).bytes
+            if not isinstance(value, uuid.UUID):
+                return uuid.UUID(hex=value).bytes
+            else:
+                return value.bytes
 
     def process_result_value(self, value, dialect):
+        print(value)
         if value is None:
             return value
         else:
-            return uuid.UUID(bytes=value).hex
-        
-    # This is a shallow copy and is provided to fulfill part of the TypeEngine contract. It usually does not need to be overridden unless the user-defined TypeDecorator has local state that should be deep-copied.
-    # def copy(self, **kw):
-    #     return HashColumn(32)
+            if not isinstance(value, uuid.UUID):
+
+                value = uuid.UUID(bytes=value)
+            return value

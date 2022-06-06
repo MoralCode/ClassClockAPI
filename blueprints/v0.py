a287cd45caad82fed474c51c97ee8a260e390554
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-06-05 22:37:20 -0400

    no need to set the id

--- blueprints/v0.py
+++ blueprints/v0.py
@@ -476,7 +476,6 @@ def update_bellschedule(bell_schedule_id):
     try:
         updated_schedule = BellScheduleSchema(exclude=('id', 'creation_date')).load(
             data, session=db.session, instance=schedule)
-        updated_schedule.id = bell_schedule_id
     except ValidationError as err:
         # print(err.messages)  # => {"email": ['"foo" is not a valid email address.']}
         # print(err.valid_data)

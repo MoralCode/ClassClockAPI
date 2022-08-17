a2fa39e50be67b117ab11f2bd04b8ee636f80f1b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-08-17 18:44:39 -0400

    specify charset and collation so mysql-connector-python doesn't get confused https://forums.mysql.com/read.php?50,677424,677981#msg-677981

--- auth.py
+++ auth.py
@@ -12,7 +12,7 @@ db_name = os.getenv("DB_NAME", "classclock")
 
 
 db_connection_string=os.getenv("DB_CONNECTION_URL", 
-    'mysql+mysqlconnector://{user}:{pw}@{url}/{db}'.format(
+    'mysql+mysqlconnector://{user}:{pw}@{url}/{db}?charset=utf8mb4&collation=utf8mb4_general_ci'.format(
         user=db_username,
         pw=db_password,
         url=db_host,

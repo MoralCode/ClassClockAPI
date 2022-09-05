f46880548908644c7a39a27cca48ef4f43bf3c09
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-09-05 10:55:00 -0400

    update management API code to use requests instead of manually using HTTP libs

--- common/services/auth0management.py
+++ common/services/auth0management.py
@@ -1,56 +1,53 @@
-import http.client
+import requests
 import json
 from os import environ as env
-
+import logging
 
 class Auth0ManagementService:
 
     base_path = "/api/v2"
+    base_url = "https://" + env.get("AUTH0_DOMAIN") + base_path 
 
     headers = {"Content-Type": "application/json"}
 
     def __init__(self):
-        self.conn = http.client.HTTPSConnection("classclock.auth0.com")
         self.access_token = self.get_token()
 
     def get_user(self, user_id):
 
-        url = Auth0ManagementService.base_path + "/users/" + user_id
+        url = Auth0ManagementService.base_url + "/users/" + user_id
         heads = {**Auth0ManagementService.headers, **
                  {"Authorization": "Bearer " + self.access_token}}
-        self.conn.request("GET", url, "", heads)
+        resp = requests.get(url, headers=heads)
 
-        res = self.conn.getresponse()
-        data = res.read()
-
-        return data.decode("utf-8")
+        data = resp.json()
+        return data
         # print(access_token)
 
     def get_roles_for_user(self, user_id):
-        url = Auth0ManagementService.base_path + "/users/" + user_id + "/roles"
+        url = Auth0ManagementService.base_url + "/users/" + user_id + "/roles"
         heads = {**Auth0ManagementService.headers, **
                  {"Authorization": "Bearer " + self.access_token}}
-        self.conn.request("GET", url, "", heads)
-        try:
-            res = self.conn.getresponse()
-            data = res.read()
-
-            return data.decode("utf-8")
-        except http.client.RemoteDisconnected as e:
-            # this tends to fail and then break all future auth0 stuff
-            # maybe its a connection keep alive problem?
-            #  
-            print("Auth0 Management API error: ", e)
-            return []
+        resp = requests.get(url, headers=heads)
+        data = resp.json()
+        return data
 
 
     def get_token(self):
-        payload = "{\"client_id\": \"" + env.get("AUTH0_CLIENT_ID") + "\" ,\"client_secret\": \"" + env.get(
-            "AUTH0_CLIENT_SECRET") + "\" ,\"audience\": \"https://classclock.auth0.com/api/v2/\",\"grant_type\": \"client_credentials\"}"
 
-        self.conn.request("POST", "/oauth/token", payload,
-                          Auth0ManagementService.headers)
+        # TODO: use the auth0 python SDK/lib for this https://github.com/auth0/auth0-python#management-sdk
+        payload = {
+            "client_id": env.get("AUTH0_CLIENT_ID"),
+            "client_secret": env.get("AUTH0_CLIENT_SECRET"),
+            "audience": self.base_url,
+            "grant_type": "client_credentials"
+        }
+        resp = requests.post("https://" + env.get("AUTH0_DOMAIN") + "/oauth/token", data=json.dumps(payload), headers=Auth0ManagementService.headers)
 
-        res = self.conn.getresponse()
-        data = res.read()
-        return json.loads(data.decode("utf-8"))["access_token"]
+        data = resp.json()
+        if data.get("error"):
+            logging.error("failed to get auth0 management API access token")
+            logging.error(data)
+            return ""
+        else:
+            return data["access_token"]
